92d9c98ce38ab5b4798adcff34b31b08bbe5ef05
Eric Sousa <ericsousa@gmail.com>
2019-05-01 07:59:30 -0300

    V2 initial commit, add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser')
+var express     = require('express')
+    app         = express()
+    bodyParser  = require('body-parser')
+    mongoose    =  require('mongoose')
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
