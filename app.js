4a8e8cd392f05ef6d59cb75e50882b5d4b1cefa2
Eric Sousa <ericsousa@gmail.com>
2019-05-03 08:42:03 -0300

    finish v2, start v3

--- app.js
+++ app.js
@@ -11,12 +11,17 @@ app.use(bodyParser.urlencoded({extended: true}))
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 })
 var Campground = mongoose.model("Campground", campgroundSchema)
 
 // Campground.create(
-//   {name: "Mountain Goat's Rest", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
+//   {
+//     name: "Granite Hill", 
+//     image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg",
+//     description: "This is a huge granite hill, no bathrooms. No water. Beautiful granite!"
+//   },
 //   function (err, campground) {
 //     if (err) {
 //       console.log(err)
@@ -27,50 +32,87 @@ var Campground = mongoose.model("Campground", campgroundSchema)
 //   })
 
 
-app.get('/', function  (req, res) {
-  res.render('landing')
-})
 
-app.get('/campgrounds', function (req, res) {
-  // Get all campgrounds from DB
-  Campground.find({}, function (err, allCampgrounds) {
-    if (err) {
-      console.log(err)
-    } else {
-      res.render('campgrounds', {campgrounds: allCampgrounds})
-    }
+  app.get('/', function  (req, res) {
+    res.render('landing')
   })
-})
 
-app.post('/campgrounds', function (req, res) {
-  // get data from form and add to campgrounds array
-  var name = req.body.name
-  var image = req.body.image
-  var newCampground = {name: name, image: image}
+// INDEX - show all campgrounds
+  app.get('/campgrounds', function (req, res) {
+    // Get all campgrounds from DB
+    Campground.find({}, function (err, allCampgrounds) {
+      if (err) {
+        console.log(err)
+      } else {
+        res.render('index', {campgrounds: allCampgrounds})
+      }
+    })
+  })
+
+// CREATE - add new  campground to DB
+  app.post('/campgrounds', function (req, res) {
+    // get data from form and add to campgrounds array
+    var name = req.body.name
+    var image = req.body.image
+    var desc = req.body.description
+    var newCampground = {name: name, image: image, description: desc}
+    
+    // Create a new campground and save to database
+    Campground.create(newCampground, function (err, newlyCreated) {
+      if (err) {
+        console.log(err)
+      } else {
+        // redirect back to campgrounds page
+        res.redirect('/campgrounds')  // default is to redirect to the .get route
+      }
+    })
+  })
+
+// NEW - show form to create new campground
+  app.get('/campgrounds/new', function (req, res) {
+    res.render('new')
+  })
+
+// SHOW - shows more info about one campground
+  app.get('/campgrounds/:id',  function (req, res) {
+    // find the campground with provided id
+    Campground.findById(req.params.id, function (err, foundCampground) {
+      if (err) {
+        console.log(err)
+      } else {
+        // render show template with  that compground
+        res.render('show', {campground: foundCampground})
+        
+      }
+    })
+  })
+
+
+// RUN SERVER --------------------------------------
+  app.listen('3000', function () {
+    console.log('YelpCamp server running')
+  })
+
+
+
+/*
+  NOTES 
+
+  RESTful Routes:       
   
-  // Create a new campground and save to database
-  Campground.create(newCampground, function (err, newlyCreated) {
-    if (err) {
-      console.log(err)
-    } else {
-      // redirect back to campgrounds page
-      res.redirect('/campgrounds')  // default is to redirect to the .get route
-    }
-  })
-})
+    (representetional state transfer)
+    (a mapping between HTTP routes and CRUD)
 
-app.get('/campgrounds/new', function (req, res) {
-  res.render('new')
-})
+    index     /dogs             GET      list all dogs
+    new       /dogs/new         GET      show add form
+    create    /dogs             POST     save new dog
+    show      /dogs/:id         GET      show details
+    edit      /dogs/:id/edit    GET      show edit form 
+    update    /dogs/:id         PUT      update dog
+    destroy   /dogs/:id         DELETE   delete dog
 
-app.listen('3000', function () {
-  console.log('YelpCamp server running')
-})
+  -----------------------------
 
+  db.nameOfCollection.drop()   >>    delete everything into the collection
 
-// Notes
-
-// RESTful route: 
-//  .get('/section')
-//  .post('/section')
-//  .get('/section/new')  // form goes here
+*/
