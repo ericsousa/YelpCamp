bcf0d5c4ade76ab0608bfd873a86c14538a4b82b
Eric Sousa <ericsousa@gmail.com>
2019-05-03 16:36:39 -0300

    custom css

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var SeedDB      = require('./seeds')
 var app = express()
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static(__dirname + '/public'))
 
 // DATABASE CONFIG 
 mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true})
@@ -23,6 +24,11 @@ SeedDB()
   app.get('/', function  (req, res) {
     res.render('landing')
   })
+
+// =========================================================
+// CAMPGROUNDS ROUTES
+// =========================================================
+
 
 // INDEX - show all campgrounds
   app.get('/campgrounds', function (req, res) {
