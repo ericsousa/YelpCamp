9f7d89ea601c270aeec0e39c74bf4f7e9691754d
Eric Sousa <ericsousa@gmail.com>
2019-05-06 16:21:00 -0300

    campground authoirization middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -57,7 +57,7 @@ router.get('/:id',  function (req, res) {
 })
 
 // EDIT 
-router.get("/:id/edit", function (req, res) {
+router.get("/:id/edit", checkCampgroundOwnership, function (req, res) {
   Campground.findById(req.params.id, function (err, campground) {
     if (err) {
       console.log(err)
@@ -68,7 +68,7 @@ router.get("/:id/edit", function (req, res) {
 })
 
 // UPDATE 
-router.put('/:id', function (req, res) {
+router.put('/:id', checkCampgroundOwnership, function (req, res) {
   // find and update the crroect campground
 
   Campground.findByIdAndUpdate(req.params.id, req.body.campground, function (err, updatedCampground) {
@@ -82,7 +82,7 @@ router.put('/:id', function (req, res) {
 })
 
 // DELETE
-router.delete('/:id', function (req, res){
+router.delete('/:id', checkCampgroundOwnership, function (req, res){
   Campground.findByIdAndRemove(req.params.id, function (err) {
     if (err) {
       console.log(err)
@@ -93,12 +93,34 @@ router.delete('/:id', function (req, res){
   })
 })
 
-// Middleware
+// Middlewares
 function isLoggedIn (req, res, next) {
   if (req.isAuthenticated()) {
     return next()
   } else {
     res.redirect('/login')
+  }
+}
+
+function checkCampgroundOwnership (req, res, next) {
+   // is user logged in
+   if (req.isAuthenticated()) {
+    // get campground data and show form
+    Campground.findById(req.params.id, function (err, campground) {
+      if (err) {
+        console.log(err)
+        res.redirect('back') 
+      } else {
+        //  does user own the campground
+        if (campground.author.id.equals(req.user._id)) {
+          next()
+        } else {
+          res.redirect('back')  
+        }
+      }
+    })
+  } else {
+    res.redirect('back')  // redirect back to the previous page
   }
 }
 
